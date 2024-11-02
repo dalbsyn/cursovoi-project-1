dffb22696bdd1251981ec4e53da8fc3127daddf2
dalbsyn <rmrfmanwoman@gmail.com>
2024-11-02 22:49:40 +0500

    Название коммита. - Элементы растягиваются как положено, хотя насчет этого немного   сомнительно - вроде как в некоторых случаях все же растягивается   странно; - Добавлен показ процентного значения ОЗУ и видеопамяти. Очень полезно   знать сколько видеопамяти потребляется в данном случае. ОЗУ, правда,   не знаю зачем добавил, так как польза от него в случае использования   нейросетей мала. Вынесен в отдельный поток, так как мешал основному   потоку работать; - Частично реализована главная функция программы - распознавание речи.   Частично, потому что надо сделать кнопки выбора языка и модели   функциональными. Вынес в отдельный поток, иначе программа вешалась; - Добавил пустую кнопку остановки процесса. Надо сделать соответственно   функциональным; - Добавил соответствующие зависимости в requirements-*.txt файлы. Оставшиеся задачи: - Сделать серверную реализацию; - Исправить элементы интерфейса; - По мелочи почистить код.

--- client.py
+++ client.py
@@ -1,6 +1,11 @@
 from tkinter import *
 from tkinter import ttk
 from tkinter import filedialog
+import psutil
+import time
+import threading
+import gpustat
+import whisper
 
 # Инициализация функций.
 
@@ -13,9 +18,30 @@ def open_file():
     '''
 
     file_location = filedialog.askopenfilename()
-    select_file_location["text"]=file_location
+    select_file_location["text"] = file_location
     print(file_location)
 
+def fetch_specs():
+    while True:
+        gpu = gpustat.GPUStatCollection.new_query()
+        ram_usage = psutil.virtual_memory()
+        available_ram_label["text"] = "Доступно ОЗУ: " + str(100 - ram_usage.percent) + "%"
+        available_vram_label["text"] = "Доступно видеопамяти:" + str(int((gpu.gpus[0].memory_used / gpu.gpus[0].memory_total) * 100)) + "%"
+        time.sleep(1)
+
+def sth():
+    ram_thread = threading.Thread(target=fetch_specs)
+    ram_thread.start()
+
+def local_process(file_location):
+    output_field.delete("1.0", END)
+    model = whisper.load_model("small")
+    result = model.transcribe(file_location)
+    output_field.insert("1.0", result["text"])
+
+def local_process_sep_thread(file_location):
+    local_process_thread = threading.Thread(target=local_process, args=(file_location,))
+    local_process_thread.start()
 '''
 Инициализация приложения:
 - главное окно: main;
@@ -40,6 +66,7 @@ process_location = ["Локально", "Удаленно"]
 
 # Рамки
 left_frame = Frame(borderwidth=2, relief="solid")
+left_spec_frame = Frame(left_frame, borderwidth=2, relief="solid")
 right_frame = Frame(borderwidth=2, relief="solid")
 
 # Левая часть - текст
@@ -50,14 +77,20 @@ setup_process = Label(left_frame, text="Настройки обработки")
 link_to_project = Label(left_frame, text="https://github.com/dalbsyn/cursovoi-project-1")
 
 # Левая часть - кнопки 
-begin_button = Button(left_frame, text="Начать")
+begin_button = Button(left_frame, text="Начать", command=lambda: local_process_sep_thread(select_file_location["text"]))
+stop_button = Button(left_frame, text="Остановить")
 select_file_button = Button(left_frame, text="Выберите файл", command=open_file)
 
-# Левая часть - прочее
+# Левая часть - низ
 languages_combobox = ttk.Combobox(left_frame, values=languages)
 models_combobox = ttk.Combobox(left_frame, textvariable="abc", values=models)
 process_location_combobox = ttk.Combobox(left_frame, values=process_location)
 
+# Левая часть
+state_label = Label(left_spec_frame, text="Состояние: OK")
+available_ram_label = Label(left_spec_frame, text="Доступно ОЗУ: Нет доступа")
+available_vram_label = Label(left_spec_frame, text="Доступно видеопамяти: Нет доступа")
+sth()
 # Правая часть - текст
 output = Label(right_frame, text="Вывод работы")
 
@@ -79,22 +112,28 @@ right_frame.grid(row=0, column=1, padx=2, pady=2)
 
 # Левая часть.
 select_file.grid(row=0,  columnspan=2, sticky="w")
-select_file_location.grid(row=1, column=0)
-select_file_button.grid(row=1, column=1)
+select_file_location.grid(row=1, column=0, sticky="w")
+select_file_button.grid(row=1, column=1, sticky="nsew")
 setup_process.grid(row=2, columnspan=2, sticky="w")
 languages_combobox.grid(row=3, column=0)
 models_combobox.grid(row=3, column=1)
-process_location_combobox.grid(row=4, columnspan=2)
-begin_button.grid(row=5, columnspan=2, sticky="w")
-link_to_docs.grid(row=6, columnspan=2, sticky="w")
-link_to_project.grid(row=7, columnspan=2, sticky="w")
+process_location_combobox.grid(row=4, columnspan=2, sticky="nsew")
+left_spec_frame.grid(row=5, columnspan=2, sticky="nsew")
+begin_button.grid(row=6, column=0, sticky="nsew")
+stop_button.grid(row=6, column=1, sticky="nsew")
+link_to_docs.grid(row=7, columnspan=2, sticky="w")
+link_to_project.grid(row=8, columnspan=2, sticky="w")
+
+state_label.grid(sticky="w")
+available_ram_label.grid(sticky="w")
+available_vram_label.grid(sticky="w")
 
 # Правая часть.
 output.grid(row=0, columnspan=2, sticky="w")
 output_field.grid(row=1, columnspan=2)
-processing_progressbar.grid(row=2, columnspan=2)
-save_to_button.grid(row=3, column=0, sticky="w")
-copy_to_button.grid(row=3, column=1, sticky="w")
+processing_progressbar.grid(row=2, columnspan=2, sticky="nsew")
+save_to_button.grid(row=3, column=0, sticky="nsew")
+copy_to_button.grid(row=3, column=1, sticky="nsew")
 
 # Окно.
 main.mainloop()
